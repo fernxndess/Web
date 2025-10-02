b817d3f8bafb94da819b20361d7674de2aed048e
fernxdess <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>
2025-10-01 23:27:58 -0300

    primeiro commit

---
+++ app/controller/products_controller.ts
@@ -0,0 +1,56 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+import Product from '#models/products'
+
+import { createProductValidator } from '#validators/products'
+
+export default class ProductsController {
+  public async index({ view }: HttpContext) {
+    const products = await Product.all()
+
+    return view.render('pages/products/index', { products })
+  }
+
+  public async show({ params, view }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
+
+    return view.render('pages/products/show', { product })
+  }
+
+  public async create({ view }: HttpContext) {
+    return view.render('pages/products/create')
+  }
+
+  public async edit({ params, view }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
+
+    return view.render('pages/products/create', { product })
+  }
+
+  public async store({ request, response }: HttpContext) {
+    const payload = await request.validateUsing(createProductValidator)
+
+    const product = await Product.create(payload)
+
+    return response.redirect().toRoute('products.show', { id: product.id })
+  }
+
+  public async update({ params, request, response }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
+
+    const payload = await request.validateUsing(createProductValidator)
+
+    product.merge(payload)
+    await product.save()
+
+    return response.redirect().toRoute('products.show', { id: product.id })
+  }
+
+  public async destroy({ params, response }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
+
+    await product.delete()
+
+    return response.redirect().toRoute('products.index')
+  }
+}
